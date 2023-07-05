8990012b1cead11b36ca574c24b982db4438135a
Manthan9057 <108115013+Manthan9057@users.noreply.github.com>
2023-07-05 23:03:15 +0530

    assingment 2

--- server.js
+++ server.js
@@ -1,43 +1,41 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-
-const app = express();
-app.use(bodyParser.json());
-
-// Sample user data
-const users = [
-  { username: 'john', password: 'password1' },
-  { username: 'mary', password: 'password2' },
-  { username: 'jane', password: 'password3' }
-];
-
-// Login endpoint
-app.post('/login', (req, res) => {
-  const { username, password } = req.body;
-
-  // Check if the username is alphanumeric and has a length between 6 and 12
-  const usernameRegex = /^[a-zA-Z0-9]{6,12}$/;
-  if (!usernameRegex.test(username)) {
-    return res.status(400).json({ error: 'Invalid username' });
+class Fruit {
+    constructor(id, name, color) {
+      this.id = id;
+      this.name = name;
+      this.color = color;
+    }
   }
-
-  // Check if the password meets the requirements
-  if (password.length < 6) {
-    return res.status(400).json({ error: 'Invalid password' });
+  
+  // Sample fruits data
+  const fruits = [
+    new Fruit(1, "Apple", "Red"),
+    new Fruit(2, "Banana", "Yellow"),
+    new Fruit(3, "Orange", "Orange"),
+    new Fruit(4, "Grapes", "Green"),
+    new Fruit(5, "Blueberry", "Blue"),
+  ];
+  
+  const express = require('express');
+  const app = express();
+  
+  // Sort fruits by color
+  function sortFruitsByColor() {
+    return fruits.sort((a, b) => {
+      if (a.color < b.color) return -1;
+      if (a.color > b.color) return 1;
+      return 0;
+    });
   }
-
-  // Check if the user exists in the sample data
-  const user = users.find(u => u.username === username && u.password === password);
-  if (!user) {
-    return res.status(401).json({ error: 'Invalid credentials' });
-  }
-
-  // User is authenticated
-  return res.status(200).json({ message: 'Login successful' });
-});
-
-// Start the server
-const port = 3000;
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+  
+  // API route to get sorted fruits by color
+  app.get('/fruits', (req, res) => {
+    const sortedFruits = sortFruitsByColor();
+    res.json(sortedFruits);
+  });
+  
+  // Start the server
+  const port = 3000;
+  app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
+  
